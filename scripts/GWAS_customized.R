677149d90b06a922a70cf5912a83839cf2f202cf
Ying Wang <yiwang@broadinstitute.org>
2021-09-03 23:12:30 -0400

    update covariate file

--- scripts/GWAS_customized.R
+++ scripts/GWAS_customized.R
@@ -16,6 +16,7 @@ option_list <- list(
   make_option(c("--bin_number"), type = "character", default = NULL, help = "The No. of bin to run GWAS"),
   make_option(c("--n_threads"), type = "character", default = 1, help = "Number of threads to run PLINK2"),
   make_option(c("--summary_bins_file"), type = "character", default = NULL, help = "Full path to output file summarising number of splitted bins for all phenotypes"),
+  make_option(c("--covfile"), type = "character", default = NULL, help = "Full path and file name to the covariate file"),
   make_option(c("--out_prefix"), type = "character", default = NULL, help = "Prefix with full path to the output file of GWAS results for each phenotype per bin")
 )
 
@@ -28,12 +29,17 @@ bin <- as.numeric(opt$bin_number)
 n_threads <- opt$n_threads
 binfile <- opt$summary_bins_file
 out_prefix <- opt$out_prefix
+covfile <- opt$covfile
 
 bin_sums <- fread(binfile)
 bins <- bin_sums[PHENO == pheno, Bins]
 
 if(bin <= bins){
-  system(paste0("plink2 --bfile ", bfile_prefix, " --pheno ", phenodir, "/", pheno, "_Bin", bin, ".txt --ci 0.95 --glm cols=chrom,pos,ref,alt,a1freq,beta,se,ci,tz,p,nobs --freq --pheno-name ", pheno, " --threads ", n_threads, " --out ", out_prefix, "_", pheno, "_Bin", bin))
+  if(covfile != "NULL"){
+    system(paste0("plink2 --bfile ", bfile_prefix, " --pheno ", phenodir, "/", pheno, "_Bin", bin, ".txt --ci 0.95 --glm cols=chrom,pos,ref,alt,a1freq,beta,se,ci,tz,p,nobs --covar ", covfile, " --covar-variance-standardize --freq --pheno-name ", pheno, " --threads ", n_threads, " --out ", out_prefix, "_", pheno, "_Bin", bin))
+  } else{
+    system(paste0("plink2 --bfile ", bfile_prefix, " --pheno ", phenodir, "/", pheno, "_Bin", bin, ".txt --ci 0.95 --glm cols=chrom,pos,ref,alt,a1freq,beta,se,ci,tz,p,nobs --freq --pheno-name ", pheno, " --threads ", n_threads, " --out ", out_prefix, "_", pheno, "_Bin", bin))
+  }
 } else{
   print("No such bin file")
 }
